22514b65d7803a7837410b69553e72983b1fab8a
Gaurav dwivedi <dwivediji.995@gmail.com>
2026-08-02 10:57:06 +0530

    Added DB_PORT for Railway MySQL

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -11,7 +12,7 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) {
     console.log("❌ Database Connection Failed");
-    console.log(err.message);
+    console.log(err);
     return;
   }
 
